6110ca512b9c4542afdeeded86fe7c6234dc751d
jaimenyoung <jnyyoung@ucdavis.edu>
2024-01-27 16:05:53 -0800

    21quadratic.py

--- 21quadratic.py
+++ 21quadratic.py
@@ -4,10 +4,10 @@ import math
 
 def quadratic(a, b, c):
 	d = ((b**2) - (4*a*c)) ** 0.5
-	if ((b**2) - (4*a*c)) > 0:
+	if(b**2) - (4*a*c) > 0:
 		x = (-b + d) / (2 * a), (-b - d) / (2 * a)
 		return x
-	elif ((b**2) - (4*a*c)) == 0:
+	elif(b**2) - (4*a*c) == 0:
 		x = (-b + d) / (2 * a)
 		return x
 	else:
