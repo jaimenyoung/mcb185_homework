308bc302a71b181edc4292cf2d4273f51a637b5e
jaimenyoung <jnyyoung@ucdavis.edu>
2024-02-06 09:31:06 -0800

    quiz

--- 38quiz.py
+++ 38quiz.py
@@ -1,2 +1,16 @@
 # Authors: Jaime Young and Madison An
 
+
+def gregory(n):
+	a = 0
+	e = 1
+	f = 0
+	for pi in range(n):
+		pi = 1 + f
+		a = a + 2
+		d = e / a
+		e = e * (-1)
+		f = f + d
+	return (pi * 4)
+
+print(gregory(100))
