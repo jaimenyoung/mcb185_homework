dd6ce9524b398fe2b1ea29ab3a88974ae758bdde
jaimenyoung <jnyyoung@ucdavis.edu>
2024-03-02 02:24:15 -0800

    unit 6

--- 64profinder.py
+++ 64profinder.py
@@ -5,42 +5,35 @@
 # Proteins must start with 'M' and end with '*'
 # Deflines must have unique identifiers
 
-# cl: python3 64profinder.py ecoli.fna.gz
+# cl: python3 64profinder.py ecoli.fna.gz 100
 
 import dogma
 import mcb185
 import sys
 
-def profinder(seq):
-	start = False
-	aa_seq = ''
-	#aa_count = 0
+file = sys.argv[1]
+min_length = int(sys.argv[2])
+
+def profinder(seq, min_length):
 	proteins = []
-	for frame in range(3):
-		for i in range(frame, len(seq)-3 +1, 3):
-			codon = seq[i:i+3]
-			aa = dogma.translate(codon)
-			if aa == 'M':
-				start = True
-			if start:
-				aa_seq += aa
-			if aa == '*':
-				if len(aa_seq) >= 100:
-					#aa_count += 1
-					proteins.append(f'{aa_seq}')
-				aa_seq = ''
-				start = False
+	for frame in dogma.translate(seq).split('*'):
+		if 'M' in frame:
+			search = frame.find('M')
+			aa_seq = frame[search:]
+			if len(aa_seq) >= min_length:
+				proteins.append(aa_seq + '*')
 	return proteins
 
 aa_count = 0
 for defline, seq in mcb185.read_fasta(sys.argv[1]):
 	defwords = defline.split()
 	name = defwords[0]
-	pos_prot = profinder(seq)
-	neg_prot = profinder(dogma.revcomp(seq))
-	for pos in pos_prot:
-		aa_count += 1
-		print(f'>{defwords[0]}-prot-{aa_count}\n{pos}')
-	for neg in neg_prot:
-		aa_count += 1
-		print(f'>{defwords[0]}-prot-{aa_count}\n{neg}')
+	pos_prot = profinder(seq, min_length)
+	neg_prot = profinder(dogma.revcomp(seq), min_length)
+	for frame in range(3):
+		for pos in pos_prot:
+			aa_count += 1
+			print(f'>{defwords[0]}-prot-{aa_count}\n{pos}')
+		for neg in neg_prot:
+			aa_count += 1
+			print(f'>{defwords[0]}-prot-{aa_count}\n{neg}')
